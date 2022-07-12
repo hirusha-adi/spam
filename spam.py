d1e5cfb07ee2f56e1e62affc1ac424f3ae8c4d64
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-07-12 23:03:14 +0530

    custom function with static typing

--- spam.py
+++ spam.py
@@ -2,9 +2,11 @@ import pyautogui
 import os
 import time
 import random
+import typing as t
 
-text = "Hey Hello World"
-for word in text.split(" "):
-    interval = float(str(random.uniform(0.15, 0.5))[:5])
-    pyautogui.write(word, interval=interval)
-    pyautogui.write(" ")
+
+def typeSentence(text: t.Optional[str] = None):
+    for word in text.split(" "):
+        interval = float(str(random.uniform(0.15, 0.5))[:5])
+        pyautogui.write(word, interval=interval)
+        pyautogui.write(" ")
